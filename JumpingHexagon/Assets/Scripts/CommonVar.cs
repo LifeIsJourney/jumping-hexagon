fd5694d9cfcdfcb75a5e7c34f51ce6d845bbcd4e
PVN-AnhNguyenThe <anh.nguyen@dena.jp>
2014-02-24 18:37:50 +0700

    enable Sound on/off

--- JumpingHexagon/Assets/Scripts/CommonVar.cs
+++ JumpingHexagon/Assets/Scripts/CommonVar.cs
@@ -6,6 +6,23 @@ public class CommonVar : MonoBehaviour {
 	private static bool activeTouch = true;
 	//public
 	public float disableTouchTime = 0.3f; //moi lan touch cach nhau 0.3s
+	public bool isMusicOn = true;
+
+	void Start()
+	{
+		//get preferences MUSIC
+		int temp = PlayerPrefs.GetInt("MusicOff");
+		if (temp == 0)
+		{
+			isMusicOn = true;
+			AudioListener.volume = 1;
+		}
+		else
+		{
+			isMusicOn = false;
+			AudioListener.volume = 0;
+		}
+	}
 
 	private IEnumerator enableTouchAfterSeconds(float second)
 	{
---
+++ JumpingHexagon/Assets/Scripts/MenuTouchController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTouchController : MonoBehaviour {
+
+	void Start()
+	{
+		//get preferences MUSIC
+		int temp = PlayerPrefs.GetInt("MusicOff");
+		if (temp == 0)
+		{
+			AudioListener.volume = 1;
+		}
+		else
+		{
+			//anim
+			GameObject.Find("menu3").GetComponent<Animator>().SetBool("onMusic",false);
+			GameObject.Find("meny3_off").GetComponent<Animator>().SetBool("onMusic",true);
+			AudioListener.volume = 0;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			Vector2 touchPos = getPointToWorld(Input.mousePosition);
+			// Duyet toan bo 4 button
+			int luuButton = 0;
+			float minDistance = float.MaxValue;
+			//Neu xa qua thi cung coi nhu khong click vao Gameobject nao.
+			float XA_QUA_ROI_EM_OI = 0.5f;
+			for (int i = 1; i <= 4; i++)
+			{
+				GameObject button = GameObject.Find("menu"+i);
+				Vector2 buttonPos = new Vector2(button.transform.position.x, button.transform.position.y);
+				float khoangCachButtonvoiDiemTouch = Vector2.Distance(buttonPos, touchPos);
+				if (minDistance > khoangCachButtonvoiDiemTouch && khoangCachButtonvoiDiemTouch < XA_QUA_ROI_EM_OI)
+				{
+					luuButton = i;
+					minDistance = khoangCachButtonvoiDiemTouch;
+				}
+			}
+
+			switch (luuButton)
+			{
+				case 1:
+				{
+					//guide
+					break;
+				}
+				case 2:
+				{
+					//single
+					break;
+				}
+				case 3:
+				{
+					int temp = PlayerPrefs.GetInt("MusicOff");
+					if (temp == 0)
+					{
+						PlayerPrefs.SetInt("MusicOff",1);
+						AudioListener.volume = 0;
+						//anim
+						GameObject.Find("menu3").GetComponent<Animator>().SetBool("onMusic",false);
+						GameObject.Find("meny3_off").GetComponent<Animator>().SetBool("onMusic",true);
+					}
+					else
+					{
+						PlayerPrefs.SetInt("MusicOff",0);
+						AudioListener.volume = 1;
+						//anim
+						GameObject.Find("menu3").GetComponent<Animator>().SetBool("onMusic",true);
+						GameObject.Find("meny3_off").GetComponent<Animator>().SetBool("onMusic",false);
+					}
+					break;
+				}
+				case 4:
+				{
+					//multi
+					Application.LoadLevel ("LocalPlayerVsPlayerScene"); 
+					break;
+				}
+			}
+		}
+	}
+
+	// Lay ve point to world (vector2) tu mousePosition (vector3)
+	private Vector2 getPointToWorld(Vector3 mousePosition)
+	{
+		Vector3 mousePointedToWorld3 = Camera.main.ScreenToWorldPoint(mousePosition);
+		return new Vector2(mousePointedToWorld3.x,mousePointedToWorld3.y);
+	}
+}
